a3fe935a24e1a7046bcb0c97e1063561de27da3a
Timur Malikin <timur.malikin@gmail.com>
2021-03-17 22:02:01 +0300

    step 4: add eslint

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,9 @@
 import readlineSync from 'readline-sync';
 
 export const game = () => {
-    console.log('Welcome to the Brain Games!');
-    const userName = readlineSync.question('May I have your name? ');
-    console.log('Hello, ' + userName + '!');
-}
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+};
+
+export default game;
